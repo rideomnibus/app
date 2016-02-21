47db16085c4eb31857c97a94076089539c3c8d92
Justin Foley <justin@modweb.io>
2016-02-20 21:53:58 -0500

    fix clearInterval for driver update

--- client/views/driver/driver.js
+++ client/views/driver/driver.js
@@ -1,15 +1,22 @@
 /* global Geolocation, Buses */
-Template.driver.onCreated(function () {
+var createInterval = function () {
   Template.instance().updateLocationInterval = Meteor.setInterval(function () {
     let latLng = Geolocation.latLng();
     Meteor.call('updateLatLng', latLng);
   }, 1000);
+};
+
+Template.driver.onCreated(function () {
+  if (isDrivingHelper()) {
+    createInterval();
+  }
 });
 
 Template.driver.events({
   'click #start-tracking-driving' (event, template) {
     let latLng = Geolocation.latLng();
     Meteor.call('startTrackingDriving', latLng);
+    createInterval();
   },
   'click #stop-tracking-driving' (event, template) {
     Meteor.call('stopTrackingDriving');
