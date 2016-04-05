0447405002bf045dcc6289fe63b483a9c8f77465
Justin Foley <justin@modweb.io>
2016-04-05 14:47:52 -0400

    scan qr code; WIP #23

--- client/views/scan/scan.js
+++ client/views/scan/scan.js
@@ -1,7 +1,15 @@
-/* globals scanner */
+/* globals cordova */
 
 Template.scan.events({
   'click .scan-barcode' (event) {
-    scanner.startScanning();
+    cordova.plugins.barcodeScanner.scan(
+      function (result) {
+        console.log(`We got a barcode\nResult: ${result.text}\nFormat: ${result.format}\nCancelled: ${result.cancelled}`);
+        Meteor.call('Tickets.methods.scan', result.text);
+      },
+      function (error) {
+        console.log(`Scanning failed: ${error}`);
+      }
+    );
   }
 });
