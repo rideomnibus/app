8e2471c64a969f5f99fb16366e3c2cee6ce02d89
Justin Foley <justin@modweb.io>
2016-04-05 16:04:57 -0400

    clean lint

--- server/checkout.js
+++ server/checkout.js
@@ -1,13 +1,15 @@
-Meteor.methods({
-    'chargeCard': function(stripeToken) {
-        var Stripe = StripeAPI('sk_test_dPXcFzmareJ9fYbte1r8ENff');
+/* globals StripeAPI */
 
-        Stripe.charges.create({
-            source: stripeToken.id,
-            amount: stripeToken.amount,
-            currency: 'usd'
-        }, function(err, charge) {
-            console.log(err, charge);
-        });
-    }
+Meteor.methods({
+  'chargeCard': function (stripeToken) {
+    var Stripe = StripeAPI('sk_test_dPXcFzmareJ9fYbte1r8ENff');
+
+    Stripe.charges.create({
+      source: stripeToken.id,
+      amount: stripeToken.amount,
+      currency: 'usd'
+    }, function (err, charge) {
+      console.log(err, charge);
+    });
+  }
 });
